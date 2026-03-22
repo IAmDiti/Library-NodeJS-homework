a32ec31484484c3f994b216c77b5d08aa6b2d176
IAmDiti <ditidon@gmail.com>
2026-03-22 02:33:15 +0100

    v1 library

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const {
     addBooks,
     getBooks,
+    deleteBook,
+    editBooks,
 } = require("./books");
 
 async function main () {
     const booksBefore = await getBooks();
-    console.log(booksBefore);
+    console.log("Before: ", booksBefore);
 
-    await addBooks({Book: "Norwegian Wood", Author: "Haruki Murakami"});
+    // await addBooks({Book: "Never let me go", Author: "Kazuo Ishiguro"});
+
+    // await deleteBook(0);
+
+    await editBooks(0, {
+        Book: "Les Miserables",
+        Author: "Victor Hugo",
+    });
+
+    const bookAfter = await getBooks();
+    console.log("After:", bookAfter);
+    
+
+
 }
+
 
 
 
 main();
 
-//999
